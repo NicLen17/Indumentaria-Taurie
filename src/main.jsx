6e4690dadc6d0aee55c3379b65c566917c613fa5
Fabio <fabioramosnic@gmail.com>
2023-09-04 16:15:22 -0300

    Cambio de default url

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
-axios.defaults.baseURL = 'http://localhost:4000/api/';
-//axios.defaults.baseURL = 'https://backendtaurie.onrender.com/api/';
+//axios.defaults.baseURL = 'http://localhost:4000/api/';
+axios.defaults.baseURL = 'https://backendtaurie.onrender.com/api/';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
