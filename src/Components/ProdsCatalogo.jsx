38c4c4b88ba1eecae8afe4ebdc4cc2632631c572
Fabio <fabioramosnic@gmail.com>
2023-09-04 12:15:19 -0300

    arreglos a todos los background de img y filtrado de productos

--- src/Components/ProdsCatalogo.jsx
+++ src/Components/ProdsCatalogo.jsx
@@ -3,23 +3,61 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
+import SearchaBar from './SearchaBar';
 
 function ProdsCatalogo() {
 
     const [products, setProducts] = useState([]);
+    const [filter, setFilter] = useState([]);
+    const [busqueda, setBusqueda] = useState('');
     const [cargando, setCargando] = useState(true);
 
     useEffect(() => {
         const productos = async () => {
             const { data } = await axios.get("productos");
             setProducts(data);
+            setFilter(data);
             setCargando(false);
         };
         productos()
     }, []);
 
+    const handleBusqueda = (e) => {
+        setBusqueda(e.target.value);
+    }
+
+    const onSearch = (searchTerm, e) => {
+        e.preventDefault();
+        const buscado = searchTerm.toString().toLowerCase();
+        const filter = products.filter((producto) => {
+            const nombreBuscado = producto.nombre.toLowerCase();
+            const generoBuscado = producto.genero.toLowerCase();
+            const talleBuscado = producto.talle.toLowerCase();
+            const materialBuscado = producto.material.toLowerCase();
+            const categoriaBuscada = producto.categoria.toLowerCase();
+            const marcaBuscada = producto.marca.toLowerCase();
+            return nombreBuscado.includes(buscado) || generoBuscado.includes(buscado) || talleBuscado.includes(buscado) || materialBuscado.includes(buscado) || categoriaBuscada.includes(buscado) || marcaBuscada.includes(buscado);
+        });
+        if (filter.length === 0) {
+            setFilter(products)
+        } else {
+            setFilter(filter);
+        }
+    }
+
+    const limpiar = () => {
+        setFilter(products);
+        setBusqueda('');
+    }
+
     return (
         <>
+            <SearchaBar limpiar={limpiar} onSearch={onSearch} handleBusqueda={handleBusqueda} busqueda={busqueda}/>
+            {
+                busqueda &&(
+                    <h2 className='Search-title'>Buscando productos: <span style={{color: "var(--decoraciones)"}}>{busqueda}</span></h2>
+                )
+            }
             {
                 cargando
                     ?
@@ -32,7 +70,7 @@ function ProdsCatalogo() {
                         {
                             <>
                                 <div className='Catalogo-container'>
-                                    {products.map((prod) => {
+                                    {filter.map((prod) => {
                                         return (
                                             <>
                                                 <div data-aos="fade-up" data-aos-delay="200" data-aos-once="true" data-aos-duration="1000" key={prod.id} className='Catalogo-cards'>
@@ -41,7 +79,7 @@ function ProdsCatalogo() {
                                                             <div className='Catalogo-img'>
                                                                 <img src={prod.imgFirst} alt="" />
                                                             </div>
-                                                            <h3> {prod.nombre} </h3>
+                                                            <h3 style={{textTransform: "capitalize"}}> {prod.nombre} </h3>
                                                             <p> <span style={{ color: "green" }}>$</span>{prod.precio} </p>
                                                         </div>
                                                     </Link>
---
+++ src/Components/SearchaBar.jsx
@@ -0,0 +1,15 @@
+import './SearchBar.css'
+
+function SearchaBar({ busqueda, handleBusqueda, onSearch, limpiar }) {
+  return (
+    <div className='Search-container'>
+      <form className='SearchBar' onSubmit={(e) => onSearch(busqueda, e)}>
+        <input placeholder='Nombre, marca, material...' value={busqueda} type="text" onChange={handleBusqueda} />
+        <button onSubmit={(e) => onSearch(busqueda, e)}> <img width={32} src="https://icongr.am/clarity/search.svg?size=128&color=000000" alt="Icono de busqueda de imagen" /></button>
+      </form>
+      <button style={{border: "1px solid var(--decoraciones)", borderRadius: "50px", width: "50px", height: "50px"}} onClick={() => limpiar()}><img width={32} src="https://icongr.am/clarity/window-close.svg?size=128&color=000000" alt="Icono de busqueda de imagen" /></button>
+    </div>
+  )
+}
+
+export default SearchaBar
